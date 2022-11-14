112aee932035e1fa2685e6f5d7a88294ffea3f1a
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2022-11-14 15:28:14 -0300

    Adicionado atributos ao Zoologico

--- app/Models/Zoologico.php
+++ app/Models/Zoologico.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Zoologico extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'cidade',
+        'pais',
+    ];
 }
--- database/migrations/2022_11_14_181427_create_zoologicos_table.php
+++ database/migrations/2022_11_14_181427_create_zoologicos_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('zoologicos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('cidade');
+            $table->string('pais');
             $table->timestamps();
         });
     }
