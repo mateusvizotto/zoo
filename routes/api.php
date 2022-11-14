36bd965c8d275d35fbb37bba8f1a89e9ff7375c0
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2022-11-14 18:53:52 -0300

    Criado algumas rotas para Zoologico

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ZoologicoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/zoologicos', [ZoologicoController::class, 'index']);
+Route::get('/zoologicos', [ZoologicoController::class, 'store']);
+Route::get('/zoologicos', [ZoologicoController::class, 'show']);
